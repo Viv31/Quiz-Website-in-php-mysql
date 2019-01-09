d3b388dda973d5045340a76ef3a2827ab26c7cac
Vaibhav Ganrade <45598521+Viv31@users.noreply.github.com>
2019-01-09 16:11:41 +0530

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,140 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style type="text/css">
+	.footer{
+		background: black;
+		color:white;
+		height: 100px;
+
+}
+.btn-fb{
+	background:#4267b2;
+	font-size: 20px;
+}
+
+.btn-fb:hover{
+	background:transparent;
+	border: 1px solid white;
+	color: white;
+	font-weight: 700;
+	
+}
+
+.btn-li:hover{
+	background:transparent;
+	border: 1px solid white;
+	color: white;
+	font-weight: 700;
+	
+}
+
+.btn-li{
+
+	background: #4875B4;
+	font-size: 20px;
+}
+.btn-you{
+	background: #FF3333;
+	font-size: 20px;
+}
+
+.btn-you:hover{
+	background:transparent;
+	border: 1px solid white;
+	color: white;
+	font-weight: 700;
+	
+}
+.fa-mobile{
+	font-size: 20px;
+}
+
+
+
+@media only screen and (max-width: 600px) 
+{
+	.footer{
+		background: black;
+		color:white;
+		height: 240px;
+		text-align: center;
+		width: 100%;
+
+}
+
+.btn-fb{
+	background:#4267b2;
+	font-size: 14px;
+}
+
+.btn-fb:hover{
+	background:transparent;
+	border: 1px solid white;
+	color: white;
+	font-weight: 700;
+	
+}
+
+.btn-li{
+
+	background: #4875B4;
+	font-size: 14px;
+}
+
+.btn-li:hover{
+	background:transparent;
+	border: 1px solid white;
+	color: white;
+	font-weight: 700;
+	
+}
+
+.btn-you{
+	background: #FF3333;
+	font-size: 14px;
+}
+
+.btn-you:hover{
+	background:transparent;
+	border: 1px solid white;
+	color: white;
+	font-weight: 700;
+	
+}
+	
+	h4,p{
+		text-align: center;
+	}
+}
+
+
+		
+		
+
+</style>
+
+<div class="footer">
+	<div class="row">
+		<div class="col-md-4">
+			<h4>Desined & Developed By:</h4>
+			<p>Vaibhav Gangrade</p>
+		</div>
+		<div class="col-md-4 ">
+			
+			<h4 class="text-center">Follow me: </h4>
+
+			 <button type="button" class="btn btn-fb"><i class="fa fa-facebook-square"></i> Facebook</button>
+			 &nbsp;
+
+			 <button type="button" class="btn btn-li"><i class="fa fa-linkedin-square"></i> Linkedin</button>
+			 &nbsp;
+
+			 <button type="button" class="btn btn-you"><i class="fa fa-youtube"></i>Youtube</button>
+		</div>
+		
+		<div class="col-md-4">
+			<h4 class="text-center">Contact me:</h4>
+			<p class="text-center"><i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp; &nbsp; <a href="mailto" >vivgangs@gmail.com</a></p>
+			<p class="text-center"><i class="fa fa-mobile" aria-hidden="true"></i>&nbsp; &nbsp; +91-8224022245</p>
+		</div>
+	</div>
+</div>
